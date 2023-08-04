a08fe20bd1141598e391deeeac6e87f313919d5c
DoctorLynch <doctor_linchenko@mail.ru>
2023-08-05 00:25:29 +0300

    HW5

--- src/keyboard.py
+++ src/keyboard.py
@@ -3,16 +3,7 @@ from src.item import Item
 
 
 class Keyboard(Item, LangMixin):
-    def __init__(self, name, price, quantity, language='EN'):
+    def __init__(self, name, price, quantity,):
         super().__init__(name, price, quantity)
-        self.__language = language
-
-    def change_lang(self):
-        if self.__language == "EN":
-            self.__language = "RU"
-        return self
-
-    @property
-    def language(self):
-        return self.__language
+        LangMixin.__init__(self)
 
